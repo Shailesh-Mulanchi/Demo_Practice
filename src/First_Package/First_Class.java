9040e13e295990dccf69c7c9f93b6f938a14454c
Shailesh01 <shaileshmulanchi93@gmail.com>
2025-04-20 10:47:06 +0530

    target and src folder commited

---
+++ src/First_Package/First_Class.java
@@ -0,0 +1,23 @@
+package First_Package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/First_Package/Second_Class.java
@@ -0,0 +1,20 @@
+
+package First_Package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Second_Class {
+
+   public static void main(String[] args) {
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\care system\\Downloads\\chrome-win64.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+	}
+
+}
